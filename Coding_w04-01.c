e9070cc43aa941c1b34b7a5eca89cbe414a5462f
bungroti <ratthavit.big@icloud.com>
2025-07-22 14:52:55 +0700

    Add files via upload

---
+++ Coding_w04-01.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {//keyword
+    printf("Hello,C token!\n");//string
+    return 0;
+}
---
+++ Coding_w04-02.c
@@ -0,0 +1,8 @@
+#include <stdio.h>//ไม่่มี<>
+
+int main() {//keyword
+    int a = 10;//ไม่มี ;
+    printf("a = %d", a);//identifier
+    return 0;//keyword
+    //ไม่มี ;
+}
