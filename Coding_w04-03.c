d62e2436d54f32190701b83e4a974fb45101f470
bungroti <ratthavit.big@icloud.com>
2025-07-22 15:00:19 +0700

    Add files via upload

---
+++ Coding_w04-03.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+char nameIntial = 'K';
+int age = 20;
+float weight = 48.5;
+char gender[] = "Female";    
+    printf("nameIntial = %c\n", nameIntial);
+    printf("age = %d\n", age);
+    printf("weight = %.1f\n", weight);
+    printf("gender = %s\n", gender);
+    return 0;
+}
---
+++ Coding_w04-04.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+    float score = 90.5; // Changed from int to float to match the value
+    printf("score = %f\n", score);
+    return 0;
+}
